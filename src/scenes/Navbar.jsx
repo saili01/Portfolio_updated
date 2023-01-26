9ffcd84327357b3426564f9c266ca07e13a89d8f
Saili Manchkar <46997364+saili01@users.noreply.github.com>
2023-01-26 20:00:05 +0530

    code fix

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion,useCycle } from "framer-motion"
